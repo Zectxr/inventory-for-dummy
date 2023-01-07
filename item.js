3d6919ffa3a964852a8ec91b1eec1ed07bc419f7
Zec <78596372+Zectxr@users.noreply.github.com>
2023-01-07 21:27:08 +0800

    Create item.js

---
+++ item.js
@@ -0,0 +1,13 @@
+package com.testing;
+public class Item {
+    private String name;
+    public Item(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public String toString() {
+        return name;
+    }  
+}
